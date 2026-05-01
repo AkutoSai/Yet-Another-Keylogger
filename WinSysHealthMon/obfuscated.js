fb364c6839083eba4dc5adffe5a9690621add569
Akuto Sai <36693722+AkutoSai@users.noreply.github.com>
2026-05-02 02:50:46 +0530

    Delete WinSysHealthMon directory

--- WinSysHealthMon/obfuscated.js
+++
@@ -1,5 +0,0 @@
-// This file can be used to further obfuscate the code
-// You can run your JS files through an obfuscator like javascriptobfuscator.com
-
-// Placeholder for obfuscated code - in practice, you'd put the actual obfuscated code here
-console.log("System optimizer module loaded");
